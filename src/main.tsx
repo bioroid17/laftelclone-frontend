a88cc0966051d2b61eeb8656c015b32bd03a0be7
bioroid17 <trace1997@ajou.ac.kr>
2026-06-17 00:50:05 +0900

    react-router-dom + chakra-ui

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import router from "./router";
+import { Provider } from "./components/ui/provider";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement,
+);
+root.render(
+  <React.StrictMode>
+    <Provider>
+      <RouterProvider router={router} />
+    </Provider>
+  </React.StrictMode>,
+);
